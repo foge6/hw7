d75d8396a125c3db43546ffad0776beb02f04090
makar <makar.t1@mail.ru>
2023-04-06 10:56:45 +0300

    new

--- home_work.py
+++ home_work.py
@@ -57,7 +57,7 @@ def print_operation_table(operation, num_rows=6, num_columns=6):
     list=[]
     for i in range(1,num_columns+1):
         for j in range(1,num_rows+1):
-            list.append(i*j)
+            list.append(operation(i,j))
         print(*list)
         list.clear()
 
